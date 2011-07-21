ebe03853ee85a1e9e95f8df0eabc0e1ef8687a41
bguina <guina_b@guinab-laptop.(none)>
2011-07-21 14:56:32 +0200

    .

--- src/modellog/models.py
+++ src/modellog/models.py
@@ -4,31 +4,7 @@ from django.http import HttpRequest
 from django.conf import settings
 from datetime import datetime, tzinfo
 from socket import gethostname
-from hashlib import sha256
-import settings
-import json
-
-def model_log(log):
-    try:
-        log['event_id']             #5.1.1. Event ID                        required
-        log['event_action_code']    #5.1.2. Event Action Code,              optional
-        log['event_date']           #5.1.3. Event Date/Time                 required
-        log['event_outcome']        #5.1.4. Event Outcome Indicator         required
-        log['user_id']              #5.2.1. User ID                         required
-        log['access_point_ip']      #5.3.2. Network Access Point ID         optional
-        log['source_id']            #5.4.2. Audit Source ID                 required
-        log['instance_id_type']     #5.5.4. Participant Object ID Type Code required
-        log['instance_id']          #5.5.6. Participant Object ID           required
-    except Exception as e:
-        raise e #TODO: I don't know
-    log_keys = log.keys()
-    log_keys.sort()
-    log_hash = sha256(settings.SECRET_KEY)
-    for key in log_keys:
-        log_hash.update(str(log[key]))
-    log['signature'] = log_hash.hexdigest()
-    #Logging
-    settings.logger.info(json.dumps(log, sort_keys = True))
+from at_log import at_log
 
 class ModelLog(models.Model):
 
@@ -73,7 +49,7 @@ class ModelLog(models.Model):
         else:
             log['event_action_code'] = self.LOG_ACTION_UPDATE
         self.__log_data_collect(log, event_id, user_id, request)
-        model_log(log)
+        at_log(log)
 
     def delete(self, event_id, user_id, request = HttpRequest()):
         log = {'event_action_code': self.LOG_ACTION_DELETE}
@@ -84,5 +60,5 @@ class ModelLog(models.Model):
         except Exception as e:
             raise e
             log['event_outcome'] = 12
-        model_log(log)
+        at_log(log)
 
